c044db5022ae994b3dd1087f175bb1212f25be6a
paulstey <paulstey@gmail.com>
2016-08-21 15:30:42 -0400

    added auto increment

--- create_db.sql
+++ create_db.sql
@@ -1,8 +1,9 @@
 CREATE TABLE purchases (
-    purchase_id     INT PRIMARY KEY,
+    purchase_id     INT PRIMARY KEY AUTO_INCREMENT,
     amount          DECIMAL(10, 2) NOT NULL,
-    date_purchased  DATE NOT NULL, 
     category        VARCHAR(255) NOT NULL,
+    date_purchased  DATE NOT NULL, 
     comment         VARCHAR(255)
 );
+
 
