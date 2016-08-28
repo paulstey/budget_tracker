c2ccd77538a696fe1393e2647496c69e0d80950b
paulstey <paulstey@gmail.com>
2016-08-28 09:16:12 -0400

    hide list

--- validate_inputs.py
+++ validate_inputs.py
@@ -1,10 +1,20 @@
 #!/usr/env/bin python
-purchase_categories = ["rent", "phone", "gym", "internet", "electricity", "utilities", "student loan", "gasoline", "entertainment", "groceries", "supplements", "clothing", "books", "car", "dining out", "alcohol", "travel", "gifts", "misc"]
+
+def get_categories(filename): 
+    categories = [line.rstrip("\n") for line in open(filename)]
+    return categories
+
 
 def valid_inputs(vals, categories):
     if vals[1] not in categories:
-        print("ERROR: \'{0}\' is not a defined purchase category.".format(vals[1]))
+        print("\nERROR: \'{0}\' is not a defined purchase category.\n".format(vals[1]))
+        print("The defined categories include:")
+        for c in categories:
+            print(c)
         res = False
     else:
         res = True 
     return res 
+
+purchase_categories = get_categories("expense_categories.txt")
+
