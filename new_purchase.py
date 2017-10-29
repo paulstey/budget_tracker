232583dbc2835e9e5d7a05103861ef10b88f4d65
paulstey <paulstey@gmail.com>
2017-10-29 13:25:12 -0400

    complete change to API; support multiple purchases

--- new_purchase.py
+++ new_purchase.py
@@ -1,101 +1,105 @@
 #!/usr/env/bin python
 
 import MySQLdb
-import argparse
 import datetime
 import os
+import sys
 from getpass import getpass
 
 # my modules
-from validate_inputs import valid_inputs 
 from validate_inputs import purchase_categories
 
 
-# Instantiate the parser
-parser = argparse.ArgumentParser(description='Optional app description')
 
-# Required positional argument
-parser.add_argument('amount', type = float,
-                    help = 'Required float positional argument')
+class Purchase:
 
-# Required positional argument
-parser.add_argument('category', type = str, 
-                    help = 'Required float positional argument')
-
-# Optional positional argument
-parser.add_argument('date_purchased', type = str, nargs = '?',
-                    help = 'Optional string positional argument')
-
-# Optional argument
-parser.add_argument('--comment', type = str,
-                    help='Optional string argument')
+    def __init__(self):
+        self.amount = get_value("amount")
+        self.category = get_value("category")
+        self.date = get_value("date")
+        self.comment = get_value("comment")
+        self.assemble_vals()
 
 
-# if no date given, this function returns todays
-def getdate(args):
-    if args.date_purchased == None:
-        res = datetime.date.today().strftime("%Y-%m-%d")
+    def assemble_vals(self):
+        vals = [self.amount, self.category, self.date, self.comment]
+        self.vals = vals
+
+
+    def is_correct(self):
+        print('Amount:   ', self.amount)
+        print('Category: ', self.category)
+        print('Date:     ', self.date)
+        print('Comment:  ', self.comment)
+        print('\n')
+        print("Is this correct? (y/N)")
+        chk = input()
+        return chk
+
+
+    def valid_inputs(self, categories):
+        if self.vals[1] not in categories:
+            print("\nERROR: \'{0}\' is not a defined purchase category.\n".format(self.vals[1]))
+            print("The defined categories include:")
+            for c in categories:
+                print(c)
+            res = False
+        else:
+            res = True
+        return res
+
+
+    def gen_query(self):
+        if self.vals[3] == "":
+            self.vals[3] = "NULL"
+        query = "INSERT INTO purchases (amount, category, date_purchased, comment) VALUES ({0}, \'{1}\', \'{2}\', \'{3}\')".format(self.vals[0], self.vals[1], self.vals[2], self.vals[3])
+        return query
+
+
+    def insert_purchase(self, con, pswd):
+        query = self.gen_query()
+        print(query)
+
+        try:
+            cur = con.cursor()
+            cur.execute(query)
+            con.commit()
+            print("Successfully inserted new purchase")
+        except MySQLdb.IntegrityError:
+            print("Failed to insert new purchase")
+
+
+
+
+
+def get_value(typ):
+    if typ == "date":
+        print("Enter date (YYYY-MM-DD): ")
+        date = input()
+        if date == "":
+            val = datetime.date.today().strftime("%Y-%m-%d")
+        else:
+            val = date
     else:
-        res = args.date_purchased
-    return res 
-
-
-def print_args(args):
-    date = getdate(args)
-    print('Amount:   ', args.amount)
-    print('Category: ', args.category)
-    print('Date:     ', date)
-    print('Comment:  ', args.comment)
-    print('\n')
+        print("Enter ", typ, ": ", sep = "")
+        val = input()
+        if val != "":
+            val = val
+    return val
 
 
 def getpassword():
     pw = getpass("Please enter your password: \n")
-    return pw 
+    return pw
 
 
-# returns a list of values in the order below
-def getvalues(args):
-    vals = []
-    vals.append(args.amount)
-    vals.append(args.category)
-    vals.append(getdate(args))
-    vals.append(args.comment)
-
-    if vals[3] == None:
-        vals[3] = "NULL"
-    return vals 
-
-
-# returns a string with our insert query
-def gen_query(vals):
-    query = "INSERT INTO purchases (amount, category, date_purchased, comment) VALUES ({0}, \'{1}\', \'{2}\', {3})".format(vals[0], vals[1], vals[2], vals[3])
-    return query 
-
-
-
-def execute_query(query, pswd):
-    # now connect to `budget` DB
-    con = MySQLdb.connect(host = "localhost",    
-                         user = "root",        
-                         passwd = pswd,  
-                         db = "budget")  
-    try:
-        cur = con.cursor()
-        cur.execute(query)
-        con.commit()
-        print("Successfully inserted new purchase")
-    except MySQLdb.IntegrityError:
-        print("Failed to insert new purchase")
-
-    return con
 
 
 
 def months_totals(con, date):
     first_of_month = date[0:8] + "01"
     query = "SELECT category, sum(amount) FROM (SELECT * FROM purchases WHERE date_purchased >= \'{0}\') tbl1 GROUP BY category;".format(first_of_month)
-    
+
     cur = con.cursor()
     cur.execute(query)
     rows = cur.fetchall()
@@ -128,33 +132,58 @@ def mysql_running():
 
 
 def main():
+    multi_purchase = sys.argv[1]
     pswd = getpassword()
-    args = parser.parse_args()
-    
-    print_args(args)
-    chk = input("Is the above correct? (y/N)\n")
-    
-    if chk != "y":
-        print("Canceling new purchase insert...")
-        return None
-    elif chk == "y": 
-        vals = getvalues(args)
-        if valid_inputs(vals, purchase_categories):
-            query = gen_query(vals)
-            con = execute_query(query, pswd)
-            months_totals(con, vals[2])
-            month_to_date_sum(con, vals[2])
-        else: 
-            print("Canceling new purchase insert...")
-    
+    today = datetime.date.today().strftime("%Y-%m-%d")
+
+
+    # now connect to `budget` DB
+    con = MySQLdb.connect(host = "localhost",
+                          user = "root",
+                          passwd = pswd,
+                          db = "budget")
+
+    if (multi_purchase == "-m") or (multi_purchase == "--multiple"):
+        another_purchase = True
+
+        while another_purchase:
+            purchase = Purchase()
+            if purchase.is_correct() == "y" and purchase.valid_inputs(purchase_categories):
+                purchase.insert_purchase(con, pswd)
+                print("Enter another purchase? (y/N)")
+                another_purchase = input() == "y"
+            else:
+                print("Canceling purchase. \n\n")
+                print("Would you like to retry? (y/N)")
+                retry = input()
+                if retry != "y":
+                    another_purchase = False
+
+    else:
+        enter_purchase = True
+
+        while enter_purchase:
+            purchase = Purchase()
+            if purchase.is_correct() == "y" and purchase.valid_inputs(purchase_categories):
+                purchase.insert_purchase(con, pswd)
+                enter_purchase = False
+            else:
+                print("Canceling purchase. \n\n")
+                print("Would you like to retry? (y/N)")
+                retry = input()
+                if retry != "y":
+                    enter_purchase = False
+
+    months_totals(con, today)
+    month_to_date_sum(con, today)
 
 
 
 if __name__ == "__main__":
+
     server_on = mysql_running()
     if not server_on:
         os.system("mysql.server start")
 
     main()
     # os.system("mysql.server stop")
-
--- validate_inputs.py
+++ validate_inputs.py
@@ -1,19 +1,9 @@
 
-def get_categories(filename): 
+def get_categories(filename):
     categories = [line.rstrip("\n") for line in open(filename)]
     return categories
 
 
-def valid_inputs(vals, categories):
-    if vals[1] not in categories:
-        print("\nERROR: \'{0}\' is not a defined purchase category.\n".format(vals[1]))
-        print("The defined categories include:")
-        for c in categories:
-            print(c)
-        res = False
-    else:
-        res = True 
-    return res 
+
 
 purchase_categories = get_categories("expense_categories.txt")
-
